6867604f24f16e0c3be376a7ecd74a2eb206dcf5
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-08-19 15:03:07 -0300

    update commit

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,5 +1,24 @@
+import { Wand } from "lucide-react";
+import { Button } from "./components/ui/button";
+
 export default function App() {
   return (
-    <h2 className="text-3xl font-bold">Hello World</h2>
+    <div>
+      <h2 className="text-3xl font-bold">Hello World</h2>
+      <Button>Click me</Button>
+      <Button variant="link">Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="destructive">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+
+      <Button size="sm">Click me</Button>
+      <Button size="lg">Click me</Button>
+      <Button size="icon"><Wand /></Button>
+
+      <Button>
+        <a href="https://www.google.com">Click me</a>
+      </Button>
+    </div>
   );
 };

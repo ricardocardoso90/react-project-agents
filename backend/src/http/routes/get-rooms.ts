701a674905519a3dd19070630350839fc582497f
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-09-04 15:45:43 -0300

    update commit

--- backend/src/http/routes/get-rooms.ts
+++ backend/src/http/routes/get-rooms.ts
@@ -1,18 +1,23 @@
 import type { FastifyInstance } from "fastify";
 
+import { count, eq } from "drizzle-orm";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
 
 export function getRoomsRoute(app: FastifyInstance) {
   app.get("/rooms", async () => {
-    const result = await db
+    const results = await db
       .select({
         id: schema.rooms.id,
         name: schema.rooms.name,
+        createdAt: schema.rooms.createdAt,
+        questionsCount: count(schema.questions.id),
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id)
       .orderBy(schema.rooms.name);
 
-    return result;
+    return results;
   });
 };

68b34689bf7e9dd05c28525763d62904e2cbf7b8
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-09-22 12:00:59 -0300

    update commit

--- frontend/src/pages/create-room.tsx
+++ frontend/src/pages/create-room.tsx
@@ -43,10 +43,12 @@ export function CreateRoom() {
                   to={`/rooms/${room.id}`}
                   className="flex items-center justify-between p-3 rounded-lg border hover:bg-accent">
 
-                  <div className="flex-1">
+                  <div className="flex-1 flex flex-col gap-1">
                     <h3 className="font-medium ">
                       {room.name}
                     </h3>
+
+                    <div className="flex items-center gap-2"></div>
                   </div>
 
                   <span className="flex items-center gap-1 text-sm">

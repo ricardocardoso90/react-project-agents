672ec56e7759088326bb87b637b8ea273a18e7ff
Ricardo Cardoso <ricardomanolo90@gmail.com>
2025-09-23 14:09:04 -0300

    update commit

--- frontend/src/components/create-room-form.tsx
+++ frontend/src/components/create-room-form.tsx
@@ -1,6 +1,27 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useForm } from "react-hook-form";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+
+const createRoomSchema = z.object({
+  name: z.string().min(3, { message: "Inclua no mínimo 3 caracteres." }),
+  description: z.string(),
+});
+
+type CreateRoomFormData = z.infer<typeof createRoomSchema>;
 
 export function CreateRoomForm() {
+  const createRoomForm = useForm<CreateRoomFormData>({
+    resolver: zodResolver(createRoomSchema)
+  });
+
+  function handleCreateRoom(data: CreateRoomFormData) {
+    console.log(data);
+  };
+
   return (
     <Card>
       <CardHeader>
@@ -9,9 +30,25 @@ export function CreateRoomForm() {
       </CardHeader>
 
       <CardContent>
-        <form className="flex flex-col gap-4">
-
-        </form>
+        <Form {...createRoomForm}>
+          <form onSubmit={createRoomForm.handleSubmit(handleCreateRoom)} className="flex flex-col gap-4">
+            <FormField
+              control={createRoomForm.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Nome da sala</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Digite o nome da sala..." />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )
+              }}
+            />
+          </form>
+        </Form>
       </CardContent>
     </Card>
   )
